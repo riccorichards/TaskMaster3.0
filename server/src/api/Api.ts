c87ce09c11e0cad0e641c4b823b7046549dccd95
Riccorichards <richard.tropimovi.1@iliauni.edu.ge>
2024-03-19 16:42:22 +0400

    fixed cookies config

--- server/src/api/Api.ts
+++ server/src/api/Api.ts
@@ -56,7 +56,7 @@ const Api = (app: Application) => {
         res.cookie("accessToken", accessToken, {
           maxAge: 3.154e10,
           httpOnly: true,
-          domain: "https://task-master3-0.vercel.app",
+          domain: "task-master3-0.vercel.app",
           path: "/",
           sameSite: "none",
           secure: true,
@@ -65,7 +65,7 @@ const Api = (app: Application) => {
         res.cookie("refreshToken", refreshToken, {
           maxAge: 3.154e10,
           httpOnly: true,
-          domain: "https://task-master3-0.vercel.app",
+          domain: "task-master3-0.vercel.app",
           path: "/",
           sameSite: "none",
           secure: true,
